d4013568d79467cc80c3cee052056db551a745ad
Alexa <alexakearns@outlook.com>
2020-08-02 14:53:56 +0100

    R: Home page loads with text 'Dev Challenge'

--- react_dashboard/src/components/app.test.js
+++ react_dashboard/src/components/app.test.js
@@ -5,3 +5,14 @@ import { shallow }from 'enzyme';
 it('renders without crashing', () => {
   shallow(<App />);
 })
+
+describe('Home page on load', () => {
+  it('Page loads with text "Dev Challenge"', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper).toIncludeText('Dev Challenge');
+  })
+})
+
+
+
+
