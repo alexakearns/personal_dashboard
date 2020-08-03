ec32cdf709647d1ae1b33e935a9a5af26aacb71c
Alexa <alexakearns@outlook.com>
2020-08-03 17:29:16 +0100

    Add sport component

---
+++ react_dashboard/src/components/sport.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Sport extends Component {
+  render() {
+    return(
+      <div>
+        <h1>SPORT!!!</h1>
+      </div>
+    );
+  };
+};
+
+export default Sport;
