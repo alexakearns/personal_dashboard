e45b044e324b5d7236ac0d8ff294678e770d6740
Alexa <alexakearns@outlook.com>
2020-08-04 10:13:24 +0100

    News parse RSS feed

--- react_dashboard/src/components/news.js
+++ react_dashboard/src/components/news.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
+import Parser from 'rss-parser';
 
 export class News extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      feed: null
+    }
+  }
+
+  async componentDidMount() {
+    let parser = new Parser();
+    const newsLink = "http://feeds.bbci.co.uk/news/rss.xml"
+    const corsProxy = "https://cors-anywhere.herokuapp.com/"
+    const feed = await parser.parseURL(corsProxy + newsLink);
+    console.log(feed.items[0])
+    
+    this.setState({feed: feed.items[0]})
+  }
 
   render() {
     return(
       <div>
         <h1>NEWS!!!</h1>
+
       </div>
     );
   };
