682831e6d538ae0a9472c7c76fb2a536190ec083
Alexa <alexakearns@outlook.com>
2020-08-05 10:35:34 +0100

    Remove Math.floor from co-ords for location acc

--- react_dashboard/src/components/weather.js
+++ react_dashboard/src/components/weather.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export class Weather extends Component {
   constructor(props) {
     super(props);
-    this.state ={
+    this.state = {
       forecast: null,
       location: null
     };
@@ -16,8 +16,8 @@ export class Weather extends Component {
     let bla = this
     let location 
     navigator.geolocation.getCurrentPosition(function(position) {
-      lat = Math.floor(position.coords.latitude)
-      lon = Math.floor(position.coords.longitude)
+      lat = position.coords.latitude
+      lon = position.coords.longitude
 
       const weatherAPI = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=da6424f2d1b1c96b8ad2199aa0ac26e5`
       console.log(weatherAPI)
@@ -25,12 +25,11 @@ export class Weather extends Component {
       .then((response) => response.json())
       .then((data) => {
         console.log(data)
-        forecast = data.main.temp
+        forecast = Math.floor((data.main.temp) - 273.15)
         location = data.name
         console.log(forecast)
         bla.setState({forecast: forecast,
         location: location})
-        console.log(this.state.forecast)
       }) 
       .catch(error => console.log(error))
     });
@@ -47,9 +46,9 @@ export class Weather extends Component {
       
     return(
       <div>
+        <h1>WEATHER!!!</h1>
         <h2>{weather.forecast}</h2>
         <h2>{weather.location}</h2>
-        <h1>WEATHER!!!</h1>
       </div>
     );
   };
