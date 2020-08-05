29c0ee4c05619ef3cc5ffd35b01b6adc95d57e70
Alexa <alexakearns@outlook.com>
2020-08-05 11:32:19 +0100

    Add bootstrap container to app - dashboard layout

--- react_dashboard/src/components/app.js
+++ react_dashboard/src/components/app.js
@@ -1,45 +1,68 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 import Weather from "./weather";
-import News from './news';
-import Sport from './sport';
-import Photos from './photos';
-import Tasks from './tasks';
-import Clothes from './clothes';
+import News from "./news";
+import Sport from "./sport";
+import Photos from "./photos";
+import Tasks from "./tasks";
+import Clothes from "./clothes";
 
-import "../app.scss"
+import "../app.scss";
+import "./dash.scss";
 
 export class App extends Component {
-
   render() {
-    
     return (
       <div>
+      <Container id="greeting">
         <div>
           <h1 id="title">Good day</h1>
         </div>
+      </Container>
         <div>
-          <Weather />
-        </div>
-        <div>
-          <News />
-        </div>
-        <div>
-          <Sport />
-        </div>
-        <div>
-          <Photos />
-        </div>
-        <div>
-          <Tasks />
-        </div>
-        <div>
-          <Clothes />
+          <Container>
+            <Row>
+              <Col>
+                <div>
+                  <Weather />
+                </div>
+              </Col>
+              <Col>
+                <div>
+                  <News />
+                </div>
+              </Col>
+              <Col>
+                <div>
+                  <Sport />
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <div>
+                  <Photos />
+                </div>
+              </Col>
+              <Col>
+                <div>
+                  <Tasks />
+                </div>
+              </Col>
+              <Col>
+                <div>
+                  <Clothes />
+                </div>
+              </Col>
+            </Row>
+          </Container>
         </div>
       </div>
-
     );
-  };
-};
+  }
+}
 
 export default App;
