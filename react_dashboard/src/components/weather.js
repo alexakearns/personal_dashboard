f90e8c3ec59108bbf809f60422bc5faa5b02f9a6
Alexa <alexakearns@outlook.com>
2020-08-05 14:30:30 +0100

    Add weather icons to render from data

--- react_dashboard/src/components/weather.js
+++ react_dashboard/src/components/weather.js
@@ -1,35 +1,40 @@
 import React, { Component } from 'react';
+// import Container from "react-bootstrap/Container";
+import "./dash.scss";
 
 export class Weather extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      forecast: null,
-      location: null
+      temperature: null,
+      location: null,
+      forecast: null
     };
   }
   
   getLocation() {
-    let forecast
+    let thisComponent = this
     let lat = 50
     let lon = 10
-    let bla = this
     let location 
+    let temperature
+    let forecast
     navigator.geolocation.getCurrentPosition(function(position) {
       lat = position.coords.latitude
       lon = position.coords.longitude
 
       const weatherAPI = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=da6424f2d1b1c96b8ad2199aa0ac26e5`
-      console.log(weatherAPI)
       fetch(weatherAPI)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data)
-        forecast = Math.floor((data.main.temp) - 273.15)
+        temperature = Math.floor((data.main.temp) - 273.15)
         location = data.name
-        console.log(forecast)
-        bla.setState({forecast: forecast,
-        location: location})
+        forecast = data.weather[0].main
+        thisComponent.setState({
+          temperature: temperature,
+          location: location, 
+          forecast: forecast
+          })
       }) 
       .catch(error => console.log(error))
     });
@@ -43,12 +48,13 @@ export class Weather extends Component {
   
   render() {
     let weather = this.state
-      
+
+
     return(
       <div>
         <h1>WEATHER!!!</h1>
-        <h2>{weather.forecast}</h2>
         <h2>{weather.location}</h2>
+        <h2>{weather.temperature}</h2>
       </div>
     );
   };
