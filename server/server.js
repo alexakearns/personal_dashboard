14b3b3a98b08c7a39648be98cea864671bd7a8fd
Alexa <alexakearns@outlook.com>
2020-08-05 17:21:51 +0100

    Add simple post request

--- server/server.js
+++ server/server.js
@@ -3,11 +3,19 @@ const app = express();
 const port = 4000
 
 
-const testing = []
+const users = []
 
-app.get('/', (req, res) => {
-  res.send(testing)
+app.use(express.json())
+
+app.get('/users', (req, res) => {
+  res.send(users)
 });
+
+app.post('/users', (req, res) => {
+  const user = {name: req.body.name, password: req.body.password}
+  users.push(user)
+  res.status(201).send()
+})
 
 app.listen(port, () => {
   console.log(`Server loaded on port ${port}`)
