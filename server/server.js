88b97c006953938e57dc8bcc8460e267b7d0e4db
Alexa <alexakearns@outlook.com>
2020-08-05 17:29:51 +0100

    Server post request with bcrypt. name, password

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
 const port = 4000
 
 
@@ -11,10 +12,16 @@ app.get('/users', (req, res) => {
   res.send(users)
 });
 
-app.post('/users', (req, res) => {
-  const user = {name: req.body.name, password: req.body.password}
-  users.push(user)
-  res.status(201).send()
+app.post('/users', async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt()
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    const user = {name: req.body.name, password: hashedPassword}
+    users.push(user)
+    res.status(201).send()
+  } catch{
+    res.status(500).send()
+  }
 })
 
 app.listen(port, () => {
