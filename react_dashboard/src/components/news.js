90c0b2876fd51ddff3ff619b311e13be72b21a28
Alexa <alexakearns@outlook.com>
2020-08-05 10:34:53 +0100

    News component renders title and content

--- react_dashboard/src/components/news.js
+++ react_dashboard/src/components/news.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import Parser from 'rss-parser';
+// import { Route } from 'react-router-dom'
+// import Newspage from './newsPage'
 
 export class News extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      feed: null
+      title: null,
+      content: null
     }
   }
 
@@ -14,16 +17,28 @@ export class News extends Component {
     const newsLink = "http://feeds.bbci.co.uk/news/rss.xml"
     const corsProxy = "https://cors-anywhere.herokuapp.com/"
     const feed = await parser.parseURL(corsProxy + newsLink);
-    console.log(feed.items[0])
+    console.log(feed)
+    console.log(feed.items[0]);
+    const topStory = feed.items[0];
     
-    this.setState({feed: feed.items[0]})
+    this.setState({
+      title: topStory.title,
+      content: topStory.content
+    })
+
+    fetch(corsProxy + newsLink)
+    .then((response) => response.text())
+    .then(contents => (contents))
   }
 
   render() {
+    let news = this.state
     return(
       <div>
-        <h1>NEWS!!!</h1>
-
+        <h1><a href="/news">NEWS!!!</a></h1>
+        {/* <Route path="/news" component={Newspage} /> */}
+        <h2>{news.title}</h2>
+        <h2>{news.content}</h2>
       </div>
     );
   };
