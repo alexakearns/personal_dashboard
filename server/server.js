444ed3d76be4404abf92f0306683e1364d89bce4
Alexa <alexakearns@outlook.com>
2020-08-05 17:40:45 +0100

    Server checks for user and checks password

--- server/server.js
+++ server/server.js
@@ -24,6 +24,22 @@ app.post('/users', async (req, res) => {
   }
 })
 
+app.post('/users/login', async (req, res) => {
+  const user = users.find( user => user.name = req.body.name)
+  if (user == null) {
+    return res.status(400).send('Cannot find user')
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send('Success')
+    } else {
+      res.send('Not Allowed')
+    }
+  } catch {
+    res.status(500).send()
+  }
+})
+
 app.listen(port, () => {
   console.log(`Server loaded on port ${port}`)
 });
