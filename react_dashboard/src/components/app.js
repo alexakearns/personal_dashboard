cd7b8a1f0aae062f9d96c6666ff55dc16991e1ef
Alexa <alexakearns@outlook.com>
2020-08-06 12:38:59 +0100

    Change App comp to include render of Login comp

--- react_dashboard/src/components/app.js
+++ react_dashboard/src/components/app.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react'
+import Login from './auth/login'
+
+import '../app.scss'
 
 export class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      loggedInStatus: "Not Logged In"
+    };
+  }
+
+  checkLoggedInStatus() {
+
+  }
+
+  componentDidMount() { 
+    this.checkLoggedInStatus();
+  }
+  
   
   render() {
-    return
+    return(
+      <div>
+        <Login />
+      </div>
+    )
   }
 }
+
+export default App;
