7493c2575da6a9ec72e84cae42d5a1eab2949f69
Alexa <alexakearns@outlook.com>
2020-08-06 12:39:35 +0100

    Add login comp with form

---
+++ react_dashboard/src/components/auth/login.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+
+import './auth.scss'
+
+export class Login extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      // location: 
+
+    }
+  }
+
+  handleSubmit() {
+
+  }
+
+  // componentWillUnmount()
+  
+    render() {
+      return(
+        <div>
+        <div id="home">
+          <h1 id="title">Dev Challenge</h1>
+        </div>
+
+          <form onSubmit={this.handleSubmit}>
+          <label>
+            Username:
+            <input type="text" name="username"/>
+          </label>
+          <label>
+            Username:
+            <input type="text" name="username"/>
+          </label>
+            <input type="image" src="../../assets/Login_button.png" alt="login" class="yellow-button"/>
+          </form>
+
+        <div>
+        <h3>New to the challenge? <button>Sign up</button></h3>
+      </div>
+      </div>
+    );
+  };
+
+}
+
+export default Login;
+
