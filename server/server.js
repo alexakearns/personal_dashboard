80bc85d8a7fcd0d88c409f073da5a7e702957763
Alexa <alexakearns@outlook.com>
2020-08-07 13:16:08 +0100

    changes to server login auth to return json to browser

--- server/server.js
+++ server/server.js
@@ -58,47 +58,17 @@ app.post("/users/signup", async (req, res) => {
     );
 });
 
-// app.post("/users/login", async (req, res) => {
-//   let { username, password } = req.body
-//   const salt = await bcrypt;
-
-//   const hashedPassword = await bcrypt.hash(salt + password, 10);
-
-//   pool.query(
-//     `SELECT * FROM users WHERE username = $1`,
-//     [username],
-//     (err, results) => {
-//       console.log(results.rows[0])
-//       if (err) {
-//         console.log(err)
-//       }
-
-//       if (results.rows.length > 0) {
-//         const user = results.row[0]
-//         bcrypt.compare(password, user.password, function(err, isMatch) {
-//           if (err) {
-//             console.log("err")
-//             if(isMatch) {
-//               console.log("Match")
-//             } else {
-//               console.log("Didn't match")
-//             }
-//           } else {
-//             console.log("no user")
-//           // console.log(results.rows[0]);
-//           }
-//       });
-//     }
-//   });
-// });
-
 app.post(
   "/users/login",
   passport.authenticate("local", {
-    successRedirect: "http://localhost:3000/login",
+    successRedirect: "/successjson",
     failureRedirect: false,
   })
 );
+
+app.get('/successjson', function(req, res) {
+  res.json({message: "Success", username: req.user})
+})
 
 app.get("/users", (req, res) => {
   pool.query(`SELECT * FROM users`, (error, results) => {
